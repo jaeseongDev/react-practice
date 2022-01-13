b066c1499ba8f8487a7df70c3e8d5e05236f9606
박재성 <qkrwotjd1445@naver.com>
2022-01-13 10:34:01 +0900

    feat: 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import './App.css';
 import Header from "./components/Header";
+import Contents from "./components/Contents";
 
 function App() {
   return (
     <div className="App">
-      {/* <Header /> */}
-      <section>
-        <h2>국내 코로나 현황</h2>
-        <div className="contents">
-
-        </div>
-      </section>
+      <Header />
+      <Contents />
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,13 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>
-      
-    </div>
+    <header className="header">
+      <h1>COVID-19</h1>
+      <select>
+        <option>국내</option>
+        <option>세계</option>
+      </select>
+    </header>
   )
 }
 
